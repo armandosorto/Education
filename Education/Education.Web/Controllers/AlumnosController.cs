1f7f4cc9d64680e386d48731cdac2754d3446761
Armando <armandosorto311@gmail.com>
2019-03-22 02:57:11 +0100

    Agregando Lista

---
+++ Education/Education.Web/Controllers/AlumnosController.cs
@@ -0,0 +1,22 @@
+﻿using Education.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Education.Web.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            var alumnos = new AlumnosModel();
+
+            var ListaAlumnos = new List<AlumnosModel>();
+            ListaAlumnos.Add();
+            return View(ListaAlumnos);
+        }
+    }
+}
---
+++ Education/Education.Web/Models/AlumnosModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Education.Web.Models
+{
+    public class AlumnosModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
