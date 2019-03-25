05f19487c39125433daeb47af2632c42cf99c715
Armando <armandosorto311@gmail.com>
2019-03-25 23:53:41 +0100

    cambios web admi grado y modalidad

---
+++ Education/Education.WebAdmin/Controllers/GradoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Education.BL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Education.WebAdmin.Controllers
+{
+    public class GradoController : Controller
+    {
+        GradoBL _gradoBL;
+        public GradoController()
+        {
+            _gradoBL = new GradoBL();
+        }
+        // GET: Grado
+        public ActionResult Index()
+        {
+            var listaGrado = _gradoBL.ObtenerGrado();
+
+            return View(listaGrado);
+        }
+
+        public ActionResult Crear()
+        {
+            var nuevoGrado = new Grado();
+
+            return View(nuevoGrado);
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Grado grado)
+        {
+            _gradoBL.GuardarGrado(grado);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Education/Education.WebAdmin/Controllers/ModalidadController.cs
@@ -0,0 +1,39 @@
+﻿using Education.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Education.WebAdmin.Controllers
+{
+    public class ModalidadController : Controller
+    {
+        ModalidadBL _modalidadBL;
+        public ModalidadController()
+        {
+            _modalidadBL = new ModalidadBL();
+        }
+        // GET: Modalidad
+        public ActionResult Index()
+        {
+            var listaModalidad = _modalidadBL.ObtenerModalidad();
+
+            return View(listaModalidad);
+        }
+
+        public ActionResult Crear()
+        {
+            var nuevoModalidad = new Modalidad();
+
+            return View(nuevoModalidad);
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Modalidad modalidad)
+        {
+            _modalidadBL.GuardarModalidad(modalidad);
+            return RedirectToAction("Index");
+        }
+    }
+}
